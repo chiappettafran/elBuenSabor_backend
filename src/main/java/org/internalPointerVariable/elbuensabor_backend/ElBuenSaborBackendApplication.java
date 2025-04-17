9b4db901eaa51d7c4289cd2df0a8d2c30fec6ca8
chiappettafran <franciscomartinezchiappetta@gmail.com>
2025-04-17 14:00:27 -0300

    Primer Commit

---
+++ src/main/java/org/internalPointerVariable/elbuensabor_backend/ElBuenSaborBackendApplication.java
@@ -0,0 +1,13 @@
+package org.internalPointerVariable.elbuensabor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElBuenSaborBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElBuenSaborBackendApplication.class, args);
+	}
+
+}
